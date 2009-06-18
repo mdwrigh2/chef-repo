e992791090be6b16ad8c13feb982253d8ad7ea21
Michael Wright <mwright@michael-wrights-macbook-pro.local>
2009-06-18 12:51:11 -0400

    Moved files to the right places

--- logrotate/definitions/logrotate.rb
+++
@@ -1,9 +0,0 @@
-define :logrotate, :frequency => "daily", :rotate_count => 5, :rotate_if_empty => false, :missing_ok => true, :compress => true, :enable => true do
-  template "/etc/logrotate.d/#{params[:name]}" do
-    action params[:enable] ? :create : :delete
-    cookbook "logrotate"
-    source "logrotate.conf.erb"
-    variables(:p => params)
-    backup false
-  end
-end
